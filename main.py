1aa89bb03310eac42b4c23551ee9220f88ef2f65
Luis Orozco <Luis.Orozco@studentambassadors.com>
2023-12-22 01:59:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def add_data():
            "username":username,
            "email":email,
            "password":password,
-           "singupDate":signUpDate
+           "signupDate":signUpDate
        })
 
        response.status_code = 201
